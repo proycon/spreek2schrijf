dc0275ada34c5ac5523e6e1475847750d8a4e2cb
Maarten van Gompel <proycon@anaproy.nl>
2017-11-20 19:26:30 +0100

    added

---
+++ spreek2schrijf/buildcorpus.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import json
+import argparse
+import glob
+
+def main():
+    parser = argparse.ArgumentParser(description="", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-i','--inputdir', type=str,help="Input directory", action='store',default=".",required=False)
+    parser.add_argument('-o','--outputprefix', type=str,help="Output prefix", action='store',default="corpus",required=False)
+    args = parser.parse_args()
+
+    spraak = open(args.outputprefix +  ".spraak.txt",'w',encoding='utf-8')
+    schrijf = open(args.outputprefix +  ".schrijf.txt",'w',encoding='utf-8')
+
+    for filename in glob.glob(args.inputdir + "/*.json"):
+        with open(filename,'rb') as f:
+            data = json.load(f)
+            for sentencepair in data['sentence_pairs']:
+                if 'asr' in sentencepair and 'transcript' in sentencepair:
+                    print(sentencepair['asr'],file=spraak)
+                    print(sentencepair['transcript'],file=schrijf)
+
+if __name__ == '__main__':
+    main()
