3177c406d0bb66230bb2db76655a2415fccc6c2e
Maarten van Gompel <proycon@anaproy.nl>
2018-01-18 16:58:51 +0100

    path fix

--- spreek2schrijf/webservice/kaldi_recog.sh
+++ spreek2schrijf/webservice/kaldi_recog.sh
@@ -2,8 +2,10 @@
 
 if [[ $(hostname) == "mlp01" ]]; then
     KALDI_main=/var/www/lamachine/src/kaldi
+    S2SDIR=/var/www/webservices-lst/live/repo/spreek2schrijf/spreek2schrijf/webservice
 elif [[ ${hostname:0:3} == "mlp" ]]; then
     KALDI_main=/vol/customopt/lamachine.dev/kaldi
+    S2SDIR=$(realpath .)
 else
     echo "Specify KALDI_main!" >&2
     exit 2
@@ -24,7 +26,7 @@ for inputfile in $inputdir/*; do
   ./decode_PR.sh $scratchdir/${file_id}.wav $target_dir
   cat $target_dir/${file_id}.txt | cut -d'(' -f 1 > $outdir/${file_id}.txt
   cp $target_dir/1Best.ctm $outdir/${file_id}.ctm
-  cat $outdir/${file_id}.ctm | perl wordpausestatistic.perl 1.0 $outdir/${file_id}.sent
+  cat $outdir/${file_id}.ctm | perl $S2SDIR/wordpausestatistic.perl 1.0 $outdir/${file_id}.sent
   ./scripts/ctm2xml.py $outdir $file_id $scratchdir
 done
 cd -
