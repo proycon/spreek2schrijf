3ef43329711622037c58cd00061288dced409334
Maarten van Gompel <proycon@anaproy.nl>
2018-01-30 12:05:10 +0100

    implemented substitutions

--- spreek2schrijf/webservice/postcorrect.py
+++ spreek2schrijf/webservice/postcorrect.py
@@ -8,6 +8,12 @@ with open(sys.argv[2],'r',encoding='utf-8') as f:
         line = line.strip()
         names[line.lower()] = line
 
+#filter duplicate punctuation
+substitutions = {
+    '. .': '.',
+    ', ,': ',',
+}
+
 with open(sys.argv[1],'r',encoding='utf-8') as f:
     for line in f:
         line = line.strip()
@@ -23,9 +29,15 @@ with open(sys.argv[1],'r',encoding='utf-8') as f:
             for l in range(4, 0,-1):
                 ngram = " ".join(words[i:i+l])
                 key = ngram.lower()
-                found = key in names
-                if found:
+                if key in names:
+                    print("  Found name " + names[key],file=sys.stderr)
+                    found = True
                     newwords += names[key].split(' ')
+                elif key in substitutions:
+                    print("  Found substitution " + key + " -> " + substitutions[key],file=sys.stderr)
+                    found = True
+                    newwords.append(substitutions[key])
+                if found:
                     skip = l-1
                     break
             if not found:
