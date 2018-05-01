73cea16bb8d4c23bbefe48e98c993b890bcee307
Maarten van Gompel <proycon@anaproy.nl>
2018-05-01 15:53:18 +0200

    added html serialiser #3

---
+++ spreek2schrijf/webservice/writeflemishhtml.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+
+txtfile, jsonfile = sys.argv[1:]
+
+with open(txtfile, 'r', encoding='utf-8') as f:
+    sentences = list(f.readlines())
+
+with open(jsonfile, 'r', encoding='utf-8') as f:
+    events = json.load(f)
+
+srcfile = events[0]['src']
+print("<html>\n<head><title>{srcfile}</title></head>\n<body><h1>{srcfile}</h1><p>\n</p>".format(srcfile=srcfile))
+
+for event in events:
+    print("<p><b> &nbsp;<font color=\"#00C000\">" + event['speaker'] + "</font></b> {"+str(event['begintime'])+"-"+str(event['endtime'])+"}<br>")
+    for sentence in event['sentences']:
+        tokens = sentences[sentence['seqnr']-1].split(' ')
+        for token in tokens:
+            print(token.strip() + "<wbr>")
+    print("</p>")
+print("</body>\n</html>")
