c045460355ee36d4e08e25ba9aedddb16b7e4b52
Maarten van Gompel <proycon@anaproy.nl>
2018-05-04 12:55:50 +0200

    On the fly fix for HTML input which suddenly changed format #3

--- spreek2schrijf/webservice/parseflemishhtml.py
+++ spreek2schrijf/webservice/parseflemishhtml.py
@@ -8,36 +8,38 @@ with open(sys.argv[1], 'r',encoding='utf-8') as f:
     doc = BeautifulSoup(f.read(), 'html.parser')
 
 srcfile = doc.title.string
-
 seqnr = 0
 events = []
-for p in doc.find_all('p'):
-    if p.font:
-        speaker = p.font.string
-
-        raw_p = str(p)
-        begintimeindex = raw_p.find('{')
-        endtimeindex = raw_p.find('}')
-        breakindex = raw_p.find('<br/>') + 6
-        begintime, endtime = [ float(x) for x in raw_p[begintimeindex+1:endtimeindex].split('-') ]
+sentences = []
+sentence = []
+begintime, endtime = 0,0
+speaker = "unknown"
+for font in doc.find('div', id='transcript').find_all('font'):
+    if 'speaker' in font['class']:
         print(srcfile, speaker, begintime, endtime,file=sys.stderr)
-        raw_body = raw_p[breakindex:len(raw_p) - 4]
-        sentence = []
-        sentences = []
-        for token in [ t.strip() for t in raw_body.split("<wbr/>") if t.strip() ]:
-            if token[-1] == '.':
-                sentence.append(token[:-1])
-                sentence.append('.')
-                seqnr += 1
+        if sentences:
+            if sentence:
                 sentences.append({'seqnr': seqnr, 'tokens': sentence})
                 sentence = []
-            else:
-                sentence.append(token)
-        if sentence: #trailing sentence
+            events.append({'speaker': speaker, 'src': srcfile, 'begintime': begintime, 'endtime': endtime, 'sentences':sentences})
+            sentences = []
+        speaker = font.get_text().strip()
+        begintime, endtime = font['ts'], font['te']
+    elif 'player' in font['class']:
+        token = font.get_text().strip()
+        if token[-1] == '.':
+            sentence.append(token[:-1])
+            sentence.append('.')
             seqnr += 1
             sentences.append({'seqnr': seqnr, 'tokens': sentence})
             sentence = []
-        events.append({'speaker': speaker, 'src': srcfile, 'begintime': begintime, 'endtime': endtime, 'sentences':sentences})
+        else:
+            sentence.append(token)
+if sentences:
+    if sentence:
+        sentences.append({'seqnr': seqnr, 'tokens': sentence})
+        sentence = []
+    events.append({'speaker': speaker, 'src': srcfile, 'begintime': begintime, 'endtime': endtime, 'sentences':sentences})
 
 with open('out.json','w',encoding='utf-8') as f:
     json.dump(events, f, indent=4, ensure_ascii=False)
